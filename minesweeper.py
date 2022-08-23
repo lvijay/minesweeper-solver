0d021156dc3f4160452320096f88fa925ac7db80
Vijay Lakshminarayanan <laksvij@hawk.iit.edu>
2022-08-23 21:39:54 +0530

    minesweeper open cell support

--- minesweeper.py
+++ minesweeper.py
@@ -3,9 +3,6 @@
 from collections import (
     defaultdict,
 )
-from fractions import (
-    Fraction,
-)
 from itertools import (
     chain,
 )
@@ -13,7 +10,7 @@ import random
 from typing import (
     Dict,
     List,
-    Optional,
+    Set,
     Tuple,
 )
 import z3
@@ -22,16 +19,27 @@ import z3
 Point = Tuple[int, int]
 
 
-def flatten(list_of_lists):
-    "Flatten one level of nesting"
-    return chain.from_iterable(list_of_lists)
+class Minesweeper:
+    MINE = 10                 # is a mine
+    UNNO = 11                 # unknown
+    EXPLODED = -1             # this board has exploded
 
-
-class Board:
-    def __init__(self, m, n):
+    def __init__(
+            self,
+            m: int,
+            n: int,
+            *,
+            minecount: int = 0):
         self.__m = m
         self.__n = n
-        self.__grid: List[bool] = [False for _ in range(m * n)]
+        self.__grid: List[int] = [Minesweeper.UNNO for _ in range(m * n)]
+        self.__mines: Dict[Point, bool] = defaultdict(lambda: False)
+        positions: List[int] = random.sample(
+            range(self.m * self.n),
+            k=minecount)
+        pos_xys: List[Point] = [self.xy(v) for v in positions]
+        for xy in pos_xys: self.__mines[xy] = True
+        self.__exploded = False
 
     @property
     def m(self): return self.__m
@@ -50,55 +58,10 @@ class Board:
         return x, y
 
     def __getitem__(self, v: Point) -> int:
-        x, y = v
-        return self.__grid[self.idx(x, y)]
+        return self.__grid[self.idx(*v)]
 
     def __setitem__(self, v: Point, val: int) -> None:
-        x, y = v
-        self.__grid[self.idx(x, y)] = val
-
-    def value_tos(self, v) -> str:
-        return '?*'[v]
-
-    def __str__(self) -> str:
-        return "\n".join(
-            "".join(self.value_tos(self[i, j]) for j in range(self.n))
-            for i in range(self.m)
-        )
-
-    def __repr__(self) -> str:
-        nl = "\n"
-        return f"<{self.__class__.__qualname__} {str(self).replace(nl, ',')}>"
-
-
-class Minesweeper(Board):
-    MINE = True
-    NOMI = False
-
-    def __init__(
-            self,
-            m: int,
-            n: int,
-            *,
-            minecount: Optional[int] = None,
-            mines: Optional[List[Point]] = None):
-        super().__init__(m, n)
-        self.__mines: Dict[Point, bool] = defaultdict(
-            lambda: False)
-        if not minecount and not mines:
-            raise ValueError("must specify one of minecount or mines")
-        if minecount and mines:
-            raise ValueError("can only specify one of minecount or mines")
-        if minecount or minecount == 0:
-            positions: List[int] = random.sample(
-                range(self.m * self.n),
-                k=minecount)
-            xys: List[Point] = [self.xy(v) for v in positions]
-            for xy in xys: self.__mines[xy] = Minesweeper.MINE
-        else:
-            for i, v in enumerate(mines): self.__mines[self.idx(i)] = v
-        self.__pstate: Dict[Point, Fraction] = {}
-        self.__exploded = False
+        self.__grid[self.idx(*v)] = val
 
     def neighbor_xys(self, x: int, y: int) -> List[Point]:
         nbrs: List[Point] = [
@@ -114,35 +77,48 @@ class Minesweeper(Board):
         return nbrs
 
     def value_tos(self, v) -> str:
-        if v == -1: return '#'
-        return super().value_tos(v) if isinstance(v, bool) else str(v)
+        if v == Minesweeper.EXPLODED: return "#"
+        if v is Minesweeper.MINE: return "*"
+        if v is Minesweeper.UNNO: return "?"
+        return str(v)
+
+    def __str__(self) -> str:
+        return "\n".join(
+            "".join(self.value_tos(self[i, j]) for j in range(self.n))
+            for i in range(self.m)
+        )
+
+    def __repr__(self) -> str:
+        nl = "\n"
+        return f"<{type(self).__qualname__} {str(self).replace(nl, ',')}>"
 
     def open(self, x, y) -> List[Tuple[Point, int]]:
         """Opens cell at (x, y).  No-op if cell already open.  Explodes if the
 cell is a mine."""
         if self.__exploded: raise ValueError("Exploded")
-        if self.__mines[x, y] == Minesweeper.MINE:
+        if self.__mines[x, y] is True:
             self._explode(x, y)
         return self._open(x, y)
 
     def _open(self, x, y) -> List[Tuple[Point, int]]:
         "Same as open but does not explode."
-        if self.__mines[x, y] == Minesweeper.MINE: return []
-        if self[x, y] is not False: return []
+        if self.__mines[x, y] is True: return []
+        if self[x, y] != Minesweeper.UNNO: return []
         self[x, y] = self._minecount(x, y)
         if self[x, y] > 0:
-            return [[(x, y), self[x, y]]]
+            return [((x, y), self[x, y])]
         # when minecount is 0 open neighbors
-        neighbors: List[Point] = self.neighbor_xys(x, y)
-        pt_count_pairs = [self._open(i, j) for i, j in neighbors]
-        return list(flatten(pt_count_pairs))
+        pt_count_pairs = [((x, y), self[x, y])]
+        for ni, nj in self.neighbor_xys(x, y):
+            pt_count_pairs.extend(self._open(ni, nj))
+        return pt_count_pairs
 
     def _explode(self, x, y):
         for i in range(self.m):
             for j in range(self.n):
                 is_mine_ij = self.__mines[i, j] is True
                 self[i, j] = True if is_mine_ij else self._minecount(i, j)
-        self[x, y] = -1
+        self[x, y] = Minesweeper.EXPLODED
         raise ValueError(f"Exploded at ({x}, {y})")
 
     def _minecount(self, x, y) -> int:
@@ -151,13 +127,107 @@ cell is a mine."""
         return sum(mines)
 
 
+class MineSolver:
+    def __init__(self, minesweeper: Minesweeper):
+        self.minesweeper = minesweeper
+        self.m, self.n = self.minesweeper.m, self.minesweeper.n
+        self.__known: Dict[Point, int] = dict()
+        self.__mines: Dict[Point, bool] = dict()
+        self.__unknowns: Set[Point] = {
+            (i, j) for i in range(self.m) for j in range(self.n)
+        }
+
+    @property
+    def known(self): return self.__known
+
+    @property
+    def mines(self): return self.__mines
+
+    @property
+    def unknowns(self): return self.__unknowns
+
+    def add_known(self, x: int, y: int, val: int) -> None:
+        self.known[x, y] = val
+        self.unknowns.remove((x, y))
+
+    def add_mine(self, x, y) -> None:
+        self.mines[x, y] = True
+        self.unknowns.remove((x, y))
+
+    def play(self, x=None, y=None):
+        if (x, y) == (None, None):
+            x, y = next(iter(set(self.unknowns)))
+        elif (x, y) in self.known:
+            return
+        print(f"opening ({x}, {y})")
+        position_vals: List[Tuple[Point, int]] = self.minesweeper.open(x, y)
+        for (px, py), mc in position_vals:
+            if (px, py) in self.known:
+                raise ValueError("unexpected value change")
+            self.add_known(px, py, mc)
+        cells: Dict[Point, z3.Int] = {
+            (ux, uy): z3.Int(f"c<{ux},{uy}>")
+            for ux, uy in self.unknowns
+        }
+        solver = z3.Solver()
+        solver.add([z3.Xor(c == 0, c == 1) for c in cells.values()])
+
+        print(known_neighbors)
+
+    def u_neighbors(self, x, y) -> List[Point]:  # unknown neighbors
+        return [
+            nxy
+            for nxy in self.minesweeper.neighbor_xys(x, y)
+            if nxy in self.unknowns
+        ]
+
+    def k_neighbors(self, x, y) -> List[Point]:  # known neighbors
+        return [
+            nxy
+            for nxy in self.minesweeper.neighbor_xys(x, y)
+            if nxy not in self.unknowns
+        ]
+
+    def __str__(self) -> str:
+        pt = self.known
+        mrows = str(self.minesweeper).split("\n")
+        prows = [
+            "".join("%d" % pt.get((i, j), 9) for j in range(self.n))
+            for i in range(self.m)
+        ]
+        return "\n".join(pr + "    " + mr for pr, mr in zip(prows, mrows))
+
+
+def dwim():
+    c00, c10, c20 = z3.Ints('c00 c10 20')
+    so = z3.Solver()
+    so.add(c00 + c10 + c20 == 1)
+    so.add(c00 + c10 == 1)
+    so.add(c00 >= 0, c00 <= 1)
+    so.add(c10 >= 0, c10 <= 1)
+    so.add(c20 >= 0, c20 <= 1)
+    so.set("sat.pb.solver", "solver")
+
+
 if __name__ == "__main__":
-    b = Minesweeper(6, 6, minecount=5); print(b._Minesweeper__mines)
-    for i in range(b.m):
-        for j in range(b.n):
-            if not b._Minesweeper__mines.get((i, j), False):
-                print(f"opening ({i},{j})")
-                b.open(i, j)
-                print(b)
+    b = Minesweeper(9, 11, minecount=3)
+    s = MineSolver(b)
+    print(s)
+    s.play()
+    print(s)
+    cells: Dict[Point, z3.Int] = {
+            (ux, uy): z3.Int(f"c<{ux},{uy}>")
+            for ux, uy in s.unknowns
+        }
+    solver = z3.Solver()
+    solver.add([z3.Xor(c == 0, c == 1) for c in cells.values()])
+    known_neighbors = {
+        uxy: [
+            nxy
+            for nxy in s.minesweeper.neighbor_xys(*uxy)
+            if nxy not in s.unknowns
+        ]
+        for uxy in s.unknowns
+    }
 
 # minesweeper.py ends here
