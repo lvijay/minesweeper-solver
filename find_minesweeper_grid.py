8c2dd7971cbac7d8a2d6bc5a2b4214c658209bd9
Vijay Lakshminarayanan <laksvij@hawk.iit.edu>
2022-09-25 08:58:46 +0530

    minor changes

--- find_minesweeper_grid.py
+++ find_minesweeper_grid.py
@@ -3,7 +3,6 @@
 from enum import Enum
 from typing import (
     Dict,
-    List,
     Iterator,
     Tuple,
     TypeVar,
@@ -37,44 +36,38 @@ class Cell(Enum):
     C8 = "8"
     UNOPENED = "UNOPENED"
     FLAG = "FLAG"
-
-    @staticmethod
-    def to_cell(val: int):
-        if val == 0: return Cell.UNOPENED
-        if val >= 1 and val <= 8: return Cell.__members__[f"C{val}"]
-        return Cell.C0
+    MINE = "MINE"
+    UNKNOWN = "UNKNOWN"
 
 
 class FindImage:
-    IMAGES = {
-        "0": image_read("find_j_0.png"),
-        "1": image_read("find_j_1.png"),
-        "2": image_read("find_j_2.png"),
-        "3": image_read("find_j_3.png"),
-        "4": image_read("find_j_4.png"),
-        "5": image_read("find_j_5.png"),
-        "6": image_read("find_j_6.png"),
-        "7": image_read("find_j_7.png"),
-        "8": image_read("find_j_8.png"),
-        "EXPLODED": image_read("find_j_exploded.png"),
-        "FINISHED": image_read("find_j_finished.png"),
-        "UNOPENED.MIDDLE": image_read("find_j_uo.png"),
-        "UNOPENED.NE": image_read("find_j_ne.png"),
-        "UNOPENED.NW": image_read("find_j_nw.png"),
-        "UNOPENED.SE": image_read("find_j_se.png"),
-        "UNOPENED.SW": image_read("find_j_sw.png"),
-    }
-
-    @staticmethod
-    def get(name) -> Image:
-        return FindImage.IMAGES[name]
-
-    @staticmethod
-    def is_cell_zero(image: Image) -> bool:
-        return (FindImage.get("0")[0][0] == image).all()
-
     def __init__(self, threshold:float=0.99):
+        image_names_files = [
+            ('0', 'find_j_0.png'), ('1', 'find_j_1.png'),
+            ('2', 'find_j_2.png'), ('3', 'find_j_3.png'),
+            ('4', 'find_j_4.png'), ('5', 'find_j_5.png'),
+            ('6', 'find_j_6.png'), ('7', 'find_j_7.png'),
+            ('8', 'find_j_8.png'), ('EXPLODED', 'find_j_exploded.png'),
+            ('FINISHED', 'find_j_finished.png'),
+            ('UNOPENED', 'find_j_uo.png'),
+            ('CORNER.NE', 'find_j_ne.png'), ('CORNER.NW', 'find_j_nw.png'),
+            ('CORNER.SE', 'find_j_se.png'), ('CORNER.SW', 'find_j_sw.png')
+        ]
+        images: Dict[str, Image] = {
+            name: image_read(filename)
+            for name, filename in image_names_files
+        }
+        image_cells = dict(
+            (('0', Cell.C0), ('1', Cell.C1), ('2', Cell.C2), ('3', Cell.C3),
+             ('4', Cell.C4), ('5', Cell.C5), ('6', Cell.C6), ('7', Cell.C7),
+             ('8', Cell.C8), ('UNOPENED', Cell.UNOPENED),))
+
+        self.__images: Dict[str, Image] = images
+        self.__image_cells: Dict[str, Cell] = image_cells
         self.__threshold: float = threshold
+
+    def __getitem__(self, name: str) -> Image:
+        return self.__images[name]
 
     @property
     def threshold(self) -> float:
@@ -91,13 +84,13 @@ class FindImage:
     def get_unopened_corner(self, image, corner) -> Tuple[
             Image, np.ndarray, np.ndarray
     ]:
-        template = FindImage.get(f"UNOPENED.{corner}")
+        template = self[f"CORNER.{corner}"]
         return template, *self.get_matches(image, template, corner)
 
     def is_game_ended(self, image) -> bool:
         "Returns True if the game is over; False otherwise."
-        exploded = FindImage.get("EXPLODED")
-        finished = FindImage.get("FINISHED")
+        exploded = self["EXPLODED"]
+        finished = self["FINISHED"]
         try:
             self.get_matches(image, exploded, "exploded") # a mine has exploded
             return True
@@ -110,48 +103,45 @@ class FindImage:
         return False
 
     def get_new_board(self, image):
-        img_ne, ne_x, ne_y = self.get_unopened_corner(image, "NE")
-        img_nw, nw_x, nw_y = self.get_unopened_corner(image, "NW")
-        img_se, se_x, se_y = self.get_unopened_corner(image, "SE")
-        img_sw, sw_x, sw_y = self.get_unopened_corner(image, "SW")
-        width_extra, height_extra = 5, 5  # FIXME hardcoding
+        _, ne_x, ne_y = self.get_unopened_corner(image, "NE")
+        _, nw_x, nw_y = self.get_unopened_corner(image, "NW")
+        _, se_x, se_y = self.get_unopened_corner(image, "SE")
+        _, sw_x, sw_y = self.get_unopened_corner(image, "SW")
         width, height = 24, 24  # FIXME hardcoding
-        nw_x, nw_y = nw_x[0] + width_extra, nw_y[0] + height_extra
+        nw_x, nw_y = nw_x[0] + 5, nw_y[0] + 5
         ne_x, ne_y = ne_x[0], ne_y[0]
         sw_x, sw_y = sw_x[0], sw_y[0]
         se_x, se_y = se_x[0], se_y[0]
         board_width = ne_x - nw_x + width
         board_height = se_y - nw_y + height
         colors = iter(((255, 0, 255), (0, 255, 0), (0, 0, 255), (255, 255, 0)))
-        for cx, cy in ((nw_x, nw_y), (ne_x, ne_y), (sw_x, sw_y), (se_x, se_y)):
-            cv2.rectangle(
-                image, (cx, cy), (cx + width, cy + height), next(colors), 1
-            )
-        cv2.imwrite("o_out.png", image)
         return Board((nw_x, nw_y), (board_width, board_height), (width, height))
 
     def identify_cell(self, cell: Image) -> Cell:
-        domain = list(range(9))
-        ideals = [FindImage.get(str(i)) for i in domain]
-        ideals[0] = FindImage.get("UNOPENED.MIDDLE")
-        def matches(cell, ideals, idx):
+        saved_cells = {
+            name: img
+            for name, img in self.__images.items()
+            if "CORNER" not in name
+        }
+        def matches(cell, saved_cell, name):
             try:
-                return self.get_matches(cell, ideals[idx], f'{idx}')
-            except Exception:
+                return self.get_matches(cell, saved_cell, name)
+            except SubImageNotFoundError:
                 return False
         match_vals = [
-            i
-            for i in domain
-            if matches(cell, ideals, i) is not False
+            name
+            for name, img in saved_cells.items()
+            if matches(cell, img, name) is not False
         ]
+        print("match_vals =", match_vals)
         if match_vals:
-            val = match_vals[-1] # preferentially take the last match
-            return Cell.to_cell(val)
-        return Cell.C0
+            name = match_vals[-1] # preferentially take the last match
+            return self.__image_cells[name]
+        return Cell.UNKNOWN
 
 
 class SubImageNotFoundError(Exception):
-    def __init__(self, name=""):
+    def __init__(self, name="image"):
         super().__init__(f"Did not find {name}")
 
 
@@ -234,8 +224,7 @@ if __name__ == "__main__":
 
     if finder.is_game_ended(image):
         print("game over")
-        exit(0)
-        pass
+        sys.exit(0)
 
     board = finder.get_new_board(image)
     print(board)
